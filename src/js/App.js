ce374ddddf0ca5b5ff3e264e7de06466ce9e06fd
mstop4 <mstop4@gmail.com>
2018-05-01 18:19:01 -0400

    added test to Card

--- src/js/App.js
+++ src/js/App.js
@@ -13,8 +13,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          <Card/>
         </p>
+        <Card/>
       </div>
     );
   }
