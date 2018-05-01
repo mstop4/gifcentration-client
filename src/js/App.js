f1583c536213ce7f77012c6cbf21b0f7a1082455
mstop4 <mstop4@gmail.com>
2018-05-01 18:14:23 -0400

    smoke test

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card';
+import logo from '../images/logo.svg';
+import '../css/App.css';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Card/>
         </p>
       </div>
     );
