87015f1e30a4cd2ecdd00f90d61e350976ef3fa2
mstop4 <mstop4@gmail.com>
2018-05-01 18:51:47 -0400

    added dom element test

--- src/js/components/Card.test.js
+++ src/js/components/Card.test.js
@@ -5,3 +5,8 @@ import Card from './Card';
 it('renders without crashing', () => {
   shallow(<Card />);
 });
+
+it('has a p', () => {
+  const card = shallow(<Card />);
+  expect(card.find('p')).toHaveLength(1);
+});
