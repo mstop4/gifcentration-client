f8fa5f06348c99a1f4b9120877e395461609bc53
mstop4 <mstop4@gmail.com>
2018-05-01 18:39:49 -0400

    setup enzyme

--- src/js/components/Card.test.js
+++ src/js/components/Card.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Card from './Card';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Card />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<Card />);
 });
