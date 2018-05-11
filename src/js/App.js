507fed3533dc10000ba75cafb764570c468caccb
mstop4 <mstop4@gmail.com>
2018-05-11 18:02:30 -0400

    restructed app a lot

--- src/js/App.js
+++ src/js/App.js
@@ -2,16 +2,59 @@ import React, { Component } from 'react'
 import MenuBar from './components/MenuBar'
 import CardArray from './components/CardArray'
 import QueryBox from './components/QueryBox'
+//import imageData from './helpers/imageData'
 import '../css/App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      imageUrls: [],
+      isLoaded: false,
+      canLoad: true,
+      loadError: null,
       numPairs: 9,
       query: 'yoshi'
     }
+
+    this.serverAddress = null
+    //this.fetchGifs = this.fetchGifs.bind(this)
   }
+
+  componentDidMount() {
+    this.serverAddress = process.env.REACT_APP_SERVER
+
+    if (this.state.canLoad) {
+      this.fetchGifs()
+    }
+  }
+
+  fetchGifs() {
+    fetch(`${this.serverAddress}/gifme/json?query=${this.state.query}&limit=${this.state.numPairs}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.length < this.state.numPairs) {
+        this.setState({
+          isLoaded: false,
+          loadError: "Query did not return enough GIFs"
+        })
+      } else {
+        this.setState({
+          imageUrls: data,
+          isLoaded: true,
+          loadError: null
+        })
+      }
+    },
+
+    error => {
+      console.dir(error)
+      this.setState({
+        isLoaded: false,
+        loadError: error.message
+      })
+    }
+  )}
 
   render() {
     return (
@@ -21,8 +64,10 @@ class App extends Component {
         />
         <MenuBar/>
         <CardArray
+          isLoaded={this.state.isLoaded}
+          loadError={this.state.loadError}
+          imageUrls={this.state.imageUrls}
           numPairs={this.state.numPairs}
-          query={this.state.query}
         />
       </div>
     );
--- src/js/components/CardArray.js
+++ src/js/components/CardArray.js
@@ -1,34 +1,31 @@
 import React, { Component } from 'react'
 import Card from './Card'
 import PropTypes from 'prop-types'
-//import imageData from '../helpers/imageData'
-import pairShuffler from '../helpers/pairShuffler' 
+import pairShuffler from '../helpers/pairShuffler'
 
 class CardArray extends Component {
   constructor() {
     super()
     this.state = {
       flipped: [],
-      imageUrls: [],
-      isLoaded: false,
-      canLoad: true,
-      error: null
     }
 
     this.numFlipped = 0
     this.cardIndices = null
     this.flippedIndices = [-1, -1]
-    this.serverAddress = null
 
     this.handleCardFlip = this.handleCardFlip.bind(this)
+    this.checkPair = this.checkPair.bind(this)
   }
 
   componentDidMount() {
-    this.serverAddress = process.env.REACT_APP_SERVER
-
-    if (this.state.canLoad) {
-      this.fetchGifs()
+    let newFlipped = []
+    for (let i = 0; i < this.state.numPairs*2; i++) {
+      newFlipped.push(true)
     }
+
+    this.setState({ flipped: newFlipped })
+    this.cardIndices = pairShuffler(this.props.numPairs)
   }
 
   handleCardFlip(index, e) {
@@ -48,43 +45,8 @@ class CardArray extends Component {
       this.setState({
         flipped: newFlipped
       })
-      console.log('Flip')
     }
   }
-
-  fetchGifs() {
-    fetch(`${this.serverAddress}/gifme/json?query=${this.props.query}&limit=${this.props.numPairs}`)
-    .then(res => res.json())
-    .then(data => {
-      if (data.length < this.props.numPairs) {
-        this.setState({
-          isLoaded: false,
-          error: "Query did not return enough GIFs"
-        })
-      } else {
-        this.cardIndices = pairShuffler(this.props.numPairs)
-
-        let newFlipped = []
-        for (let i = 0; i < this.props.numPairs*2; i++) {
-          newFlipped.push(true)
-        }
-        this.setState({
-          flipped: newFlipped,
-          imageUrls: data,
-          isLoaded: true,
-          error: null
-        })
-      }
-    },
-
-    error => {
-      console.dir(error)
-      this.setState({
-        isLoaded: false,
-        error: error.message
-      })
-    }
-  )}
 
   checkPair() {
     if (this.cardIndices[this.flippedIndices[0]] === this.cardIndices[this.flippedIndices[1]]) {
@@ -101,19 +63,19 @@ class CardArray extends Component {
   }
 
   render() {
-    if (this.state.error) {
-      return <p>Problem getting GIFs from the server: {this.state.error}</p>
+    if (this.props.loadError) {
+      return <p>Problem getting GIFs from the server: {this.props.loadError}</p>
     } else {
       let cardArray = [];
 
-      if (this.state.isLoaded) {
+      if (this.props.isLoaded) {
         for (let i = 0; i < this.props.numPairs*2; i++) {
           cardArray.push(
             <Card key={i}
                   index={i}
                   handleClick={this.handleCardFlip}
                   flipped={this.state.flipped[i]}
-                  imageUrl={this.state.imageUrls[this.cardIndices[i]]}
+                  imageUrl={this.props.imageUrls[this.cardIndices[i]]}
                   altText='kitty'
           />)
         }
@@ -129,8 +91,10 @@ class CardArray extends Component {
 }
 
 CardArray.propTypes = {
-  numPairs: PropTypes.number,
-  query: PropTypes.string
+  isLoaded: PropTypes.bool,
+  loadError: PropTypes.string,
+  imageUrls: PropTypes.array,
+  numPairs: PropTypes.number
 }
 
 export default CardArray
