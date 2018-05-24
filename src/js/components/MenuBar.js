b3ae64b12db1a5201407354bac6a665775f2d10a
mstop4 <mstop4@gmail.com>
2018-05-24 12:28:51 -0400

    responsive menubar

--- src/js/components/MenuBar.js
+++ src/js/components/MenuBar.js
@@ -4,6 +4,41 @@ import '../../css/MenuBar.css'
 
 class MenuBar extends Component {
 
+  constructor() {
+    super()
+
+    this.handleWindowResize = this.handleWindowResize.bind(this)
+  }
+
+  componentWillMount() {
+    this.handleWindowResize()
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize)
+    window.addEventListener('orientationchange', this.handleWindowResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize)
+    window.removeEventListener('orientationchange', this.handleWindowResize)
+  }
+
+  handleWindowResize() {
+    const w = window
+    const doc = document
+    const docElem = doc.documentElement
+    const menubarElems = doc.getElementsByClassName('menubar')
+
+    if (menubarElems.length > 0) {
+      const menubarStyles = w.getComputedStyle(menubarElems[0])
+
+      if (menubarStyles) {
+        docElem.style.setProperty('--menubar-height', menubarStyles.height)
+      }
+    }
+  }
+
   render() {
     return (
       <div className="menubar">
