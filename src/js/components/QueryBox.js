ae22333933c89a2757c99b2debd396b441391e50
mstop4 <mstop4@gmail.com>
2018-06-12 01:28:56 -0400

    moved PopularSearches to its own component

--- src/js/components/QueryBox.js
+++ src/js/components/QueryBox.js
@@ -7,6 +7,7 @@ import PopSearchChip from './PopSearchChip'
 import TrendingChip from './TrendingChip'
 import fetchStatus from '../helpers/fetchStatus'
 import '../../css/QueryBox.css'
+import PopularSearches from './PopularSearches';
 
 class QueryBox extends Component {
   constructor() {
@@ -21,31 +22,6 @@ class QueryBox extends Component {
   }
 
   render() {
-    let searchChips = []
-
-    searchChips.push(
-      <TrendingChip
-        key={"$trending"}
-        handleClick={this.props.handleTrendingClick}
-      />
-    )
-
-    if (this.props.popularSearches) {
-      let chipCount = 0
-
-      this.props.popularSearches.every((query) => {
-        searchChips.push(
-          <PopSearchChip
-            key={query._id}
-            label={query._id}
-            handleClick={this.props.handleChipClick}
-          />
-        )
-        chipCount++
-        return chipCount < this.maxChips
-      })
-    }
-
     const showLoading = this.props.imagesFinished && this.props.fetchStatus === fetchStatus.pending
 
     // Query toggle class
@@ -74,9 +50,12 @@ class QueryBox extends Component {
           imageLoaded={this.props.imageLoaded}
         />
         {!showLoading &&
-          <div className="query-popSearches">
-            {searchChips}
-          </div>
+          <PopularSearches
+            handlePopularClick={this.handleChipClick}
+            handleTrendingClick={this.props.handleTrendingClick}
+            popularSearches={this.props.popularSearches}
+            maxChips={this.maxChips}
+          />
         }
       </div>
     )
